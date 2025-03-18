280e75b93b9cb104ba82a85d9d49baa765abfeb6
adam shulman <vassassinx@gmail.com>
2025-03-18 14:07:00 +0200

    Data moved to file.

--- TappWidgetExtension/TappWidgetExtension.swift
+++ TappWidgetExtension/TappWidgetExtension.swift
@@ -36,13 +36,6 @@ struct SpinProvider: TimelineProvider {
     }
 }
 
-struct SpinEntry: TimelineEntry {
-    
-    let date: Date
-    let state : SpinEntryState
-    
-}
-
 @available(iOS 17, *)
 struct WheelOfCoinsWidget: Widget {
     
@@ -69,73 +62,10 @@ struct WheelOfCoinsWidget: Widget {
     }
 }
 
-enum SpinEntryState: Equatable {
-    
-    case start
-    case spinning(model: SpiningWheelStateModel)
-    case collect(prizes: [String])
-    case unAvailable
-    case noNetwork
-    
-    
-    static func ==(lhs: SpinEntryState, rhs: SpinEntryState) -> Bool {
-        switch (lhs, rhs) {
-        case (.start, .start):
-            return true
-        case (.spinning, .spinning):
-            return true
-        
-        case let (.collect(leftValue), .collect(rightValue)):
-            return true
-            //MARK: or return leftValue == rightValue
-        case (.unAvailable, .unAvailable):
-            return true
-        default:
-            return false
-        }
-    }
-    
-}
-
-extension SpinEntryState: RawRepresentable {
-
-    public typealias RawValue = String
-
-    public init?(rawValue: RawValue) {
-        switch rawValue {
-        case "start": self = .start
-        case "spinning": self = .spinning(model: SpiningWheelStateModel(rotationAngle: 0.0, firstOpacity: 0.0, secondOpacity: 0.0))
-        case "collect": self = .collect(prizes: [])
-        case "unAvailable": self = .unAvailable
-        case "noNetwork": self = .noNetwork
-        default:
-            return nil
-        }
-    }
-
-    public var rawValue: RawValue {
-        switch self {
-        case .start: return "start"
-        case .spinning(model: let model): return "spinning"
-        case .collect(prizes: let prizes): return "collect"
-        case .unAvailable: return "unAvailable"
-        case .noNetwork: return "noNetwork"
-        }
-    }
-}
-
-struct SpiningWheelStateModel {
-    
-    let rotationAngle: CGFloat
-    let firstOpacity: CGFloat
-    let secondOpacity: CGFloat
-    
-}
-
 #Preview(as: .systemSmall) {
     WheelOfCoinsWidget()
 } timeline: {
-    SpinEntry(date: Date.now, state: .collect(prizes: ["g3"]))
+    SpinEntry(date: Date.now, state: .start)
 }
 
 
